0d486e9f11fe04078eab5d3f47f8b3efb6c8df00
PhD Le Toan Thang <106635733+PhDLeToanThang@users.noreply.github.com>
2024-05-12 21:24:42 +0700

    Update s2_setup_bbb.sh

--- 2.7.7/s2_setup_bbb.sh
+++ 2.7.7/s2_setup_bbb.sh
@@ -13,61 +13,15 @@ cd ~
 ############### Tham số cần thay đổi ở đây ###################
 echo "FQDN: e.g: demo.company.vn"   # Đổi địa chỉ web thứ nhất Website Master for Resource code - để tạo cùng 1 Source code duy nhất 
 read -e FQDN
-echo "dbname: e.g: ucsdata"   # Tên DBNane
-read -e dbname
-echo "dbuser: e.g: userdata"   # Tên User access DB UCSuser
-read -e dbuser
-echo "Database Password: e.g: P@$$w0rd-1.22"
-read -s dbpass
-echo "phpmyadmin folder name: e.g: phpmyadmin"   # Đổi tên thư mục phpmyadmin khi add link symbol vào Website 
-read -e phpmyadmin
-echo "UCs Folder Data: e.g: ucsdata"   # Tên Thư mục Data vs Cache
-read -e FOLDERDATA
-echo "dbtype name: e.g: mariadb"   # Tên kiểu Database
-read -e dbtype
-echo "dbhost name: e.g: localhost"   # Tên Db host connector
-read -e dbhost
+
 echo "Email Address of Admin CA SSL/TLS"   # Địa chỉ email của nhà quản trị SSL/TLS cho HTTPS
 read -e emailssl
-
-
-GitGLPIversion="10.0.15"
 
 echo "run install? (y/n)"
 read -e run
 if [ "$run" == n ] ; then
   exit
 else
-
-#Step 1. Install NGINX
-sudo apt-get update
-sudo apt-get install nginx
-sudo systemctl stop nginx.service 
-sudo systemctl start nginx.service 
-sudo systemctl enable nginx.service
-
-#Step 2. Install MariaDB/MySQL
-#Run the following commands to install MariaDB database for Moode. You may also use MySQL instead.
-sudo apt-get install mariadb-server mariadb-client
-
-#Like NGINX, we will run the following commands to enable MariaDB to autostart during reboot, and also start now.
-sudo systemctl stop mysql.service 
-sudo systemctl start mysql.service 
-sudo systemctl enable mysql.service
-
-#Run the following command to secure MariaDB installation.
-sudo mysql_secure_installation
-
-#You will see the following prompts asking to allow/disallow different type of logins. Enter Y as shown.
-# Enter current password for root (enter for none): Just press the Enter
-# Set root password? [Y/n]: Y
-# New password: Enter password
-# Re-enter new password: Repeat password
-# Remove anonymous users? [Y/n]: Y
-# Disallow root login remotely? [Y/n]: N
-# Remove test database and access to it? [Y/n]:  Y
-# Reload privilege tables now? [Y/n]:  Y
-# After you enter response for these questions, your MariaDB installation will be secured.
 
 #Step 3. 
 wget -qO- https://raw.githubusercontent.com/bigbluebutton/bbb-install/v2.7.x-release/bbb-install.sh | bash -s -- -w -v focal-270 -s $FQDN -e $emailssl -g

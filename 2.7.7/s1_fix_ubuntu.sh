865c4632741480d67171545070fcc9e2f777d29f
PhD Le Toan Thang <106635733+PhDLeToanThang@users.noreply.github.com>
2024-05-12 21:22:43 +0700

    Update s1_fix_ubuntu.sh

--- 2.7.7/s1_fix_ubuntu.sh
+++ 2.7.7/s1_fix_ubuntu.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 clear
 sudo apt update -y
 sudo apt list --upgradable
@@ -52,4 +52,18 @@ sudo ufw allow 3389
 sudo ufw allow ssh
 
 #sudo ufw enable 
-sudo reboot
+#Cài đặt gói phần mềm để sử dụng gói apt qua giao thức HTTPS
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+
+#Thêm kho lưu trữ Docker GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+#Thêm kho lưu trữ Docker vào danh sách nguồn
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+#Cập nhật lại danh sách gói phần mềm và cài đặt Docker:
+sudo apt update -y
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+# Sau khi cài đặt, Docker sẽ tự động khởi động. 
+# Bạn có thể thêm người dùng hiện tại vào nhóm docker để có quyền sử dụng Docker mà không cần sử dụng lệnh sudo
+sudo usermod -aG docker $USER
